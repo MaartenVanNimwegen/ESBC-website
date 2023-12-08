f35104c99a752df39f06576ba2a5f26c1364f971
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-12-08 19:45:48 +0100

    styling update

--- resources/views/layouts/games.blade.php
+++ resources/views/layouts/games.blade.php
@@ -1,12 +1,27 @@
 <div class="row g-0 m-5">
     <div class="col-md-6">
-        <div class="m-2">
-            <h1 class="game">x12</h1>
+        <div class="m-2 game">
+            <h1>x12</h1>
+            <div class="row g-0">
+                <div class="col-5">
+                    <p>ESBC Menhir</p>
+                    <img src="{{ asset('storage/images/logo.png') }}" height="200px">
+                    <br>
+                </div>
+                <div class="col-2"><p>vs</p></div>
+                <div class="col-5">
+                    <p>BV Groningen</p>
+                    <img src="{{ asset('storage/images/logoBVG.png') }}" height="200px">
+                    <br>
+                </div>
+            </div>
         </div>
     </div>
+
+
     <div class="col-md-6">
-        <div class="m-2">
-            <h1 class="game">x12</h1>
+        <div class="m-2 game">
+            <h1>MSE</h1>
         </div>
     </div>
 </div>
