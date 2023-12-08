31307c7432b0e159bf33b5f74e4ac17d5081a7cb
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-12-08 14:55:16 +0100

    sass install

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/scss/app.scss', 'resources/css/app.css'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      // Define the entry point for your custom JavaScript file
+      input: {
+        app: 'resources/js/app.js',
+      },
+    },
+    // Define the output directory for the compiled JavaScript
+    outDir: 'public/js',
+  },
 });
