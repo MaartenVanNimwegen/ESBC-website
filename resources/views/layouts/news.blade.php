4e634d7f3a910c8979b8a03231eeeb36ff9fd1b9
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-12-08 23:00:15 +0100

    WIP showing news from db

--- resources/views/layouts/news.blade.php
+++ resources/views/layouts/news.blade.php
@@ -2,70 +2,20 @@
 <div class="row g-0">
     <div class="col-12 p-5">
         <div class="slick-slider">
-            <div class="news m-2">
-                <div class="row g-0 p-3">
-                    <div class="col-12">
-                        <h1>Title 1</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-                            commodi
-                            porro dignissimos iste sunt? Hic minima sed aliquam aliquid facere
-                            eum nihil
-                            unde non placeat et? Tenetur cumque eaque sunt.</p>
+            @foreach ($news as $newsItem)
+                <div class="news m-2">
+                    <div class="row g-0 p-3">
+                        <div class="col-12">
+                            <h1>{{ $newsItem->title }}</h1>
+                            <p>{{ $newsItem->description }}</p>
+                        </div>
+                    </div>
+                    <div class="row g-0">
+                        <div class="col-10"></div>
+                        <div class="col-2 date-block">{{ date_format($newsItem->created_at, "j M") }}</div>
                     </div>
                 </div>
-                <div class="row g-0">
-                    <div class="col-10"></div>
-                    <div class="col-2 date-block">8 dec</div>
-                </div>
-            </div>
-            <div class="news m-2">
-                <div class="row g-0 p-3">
-                    <div class="col-12">
-                        <h1>Title 2</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-                            commodi
-                            porro dignissimos iste sunt? Hic minima sed aliquam aliquid facere
-                            eum nihil
-                            unde non placeat et? Tenetur cumque eaque sunt.</p>
-                    </div>
-                </div>
-                <div class="row g-0">
-                    <div class="col-10"></div>
-                    <div class="col-2 date-block">8 dec</div>
-                </div>
-            </div>
-            <div class="news m-2">
-                <div class="row g-0 p-3">
-                    <div class="col-12">
-                        <h1>Title 3</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-                            commodi
-                            porro dignissimos iste sunt? Hic minima sed aliquam aliquid facere
-                            eum nihil
-                            unde non placeat et? Tenetur cumque eaque sunt.</p>
-                    </div>
-                </div>
-                <div class="row g-0">
-                    <div class="col-10"></div>
-                    <div class="col-2 date-block">8 dec</div>
-                </div>
-            </div>
-            <div class="news m-2">
-                <div class="row g-0 p-3">
-                    <div class="col-12">
-                        <h1>Title 4</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-                            commodi
-                            porro dignissimos iste sunt? Hic minima sed aliquam aliquid facere
-                            eum nihil
-                            unde non placeat et? Tenetur cumque eaque sunt.</p>
-                    </div>
-                </div>
-                <div class="row g-0">
-                    <div class="col-10"></div>
-                    <div class="col-2 date-block">8 dec</div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
