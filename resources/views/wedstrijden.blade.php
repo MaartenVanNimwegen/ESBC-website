5282cd27916783348ae8e82463465eb9de86d4e5
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-12-11 15:18:20 +0100

    WIP teams page and wedstrijden page

---
+++ resources/views/wedstrijden.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="nl">
+
+<body>
+    @extends('layouts.app')
+    @section('content')
+        <div class="nav-background"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>test wedstrijden</h1>
+                </div>
+            </div>
+        </div>
+    @endsection
+</body>
+
+</html>
