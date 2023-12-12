9922340b811516e8fbaa5e6872eadc1708b17f76
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-12-12 20:56:11 +0100

    privacy policy

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -9,7 +9,161 @@
             <div class="row">
                 <div class="col-12">
                     <h1 class="text-uppercase fw-bold">privacy policy</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem repellat impedit enim dicta aspernatur, commodi iste quod temporibus debitis nostrum expedita placeat corporis illo corrupti obcaecati rerum praesentium. Rerum, possimus.</p>
+                    <p class="fw-bold">Privacyverklaring</p>
+                    <p>Dit is de privacyverklaring van ESBC Menhir gevestigd te Scherwolderhemstraat 25, 8608 CD Sneek,
+                        ingeschreven in het handelsregister van de Kamer van Koophandel onder nummer: 40001419, hierna te
+                        noemen: <b>'de vereniging'</b>.</p>
+                    <p>Deze privacyverklaring is het laatst aangepast op 6 februari 2022.</p>
+                    <p>Bewust of onbewust deelt u persoonsgegevens met ons. Wij vinden het van belang dat zorgvuldig wordt
+                        omgegaan met uw persoonsgegevens. Daarom hebben wij in deze verklaring voor u uiteengezet hoe wij
+                        met uw persoonsgegevens omgaan en welke rechten u heeft. Heeft u vragen naar aanleiding van het
+                        lezen van deze verklaring, stel uw vraag dan gerust per e-mail aan:
+                        <b>privacy@esbcmenhir.nl</b>.
+                    </p>
+                    <p>In onderstaande tabel kunt u snel en eenvoudig terugvinden met welk doel we welke persoonsgegevens
+                        van u verzamelen, hoelang wij deze bewaren en wie deze persoonsgegevens eventueel van ons ontvangen.
+                    </p>
+                    <div class="table-responsive">
+
+                        <table class="table text-center">
+                            <thead class="table-head table-black">
+                                <tr>
+                                    <td>Doel</td>
+                                    <td>Welke persoonsgegevens</td>
+                                    <td>Grondslag</td>
+                                    <td>Bewaartermijn</td>
+                                </tr>
+                            </thead>
+                            <tbody class="table-gray">
+                                <tr>
+                                    <td>Onderzoeken of u lid kan worden en het uitvoeren van de lidmaatschapsovereenkomst.
+                                    </td>
+                                    <td>Voornaam, Achternaam, Geslacht, Adres Geboortedatum, Geboorteland, Telefoonnummer en
+                                        E-mail adres</td>
+                                    <td>Uitvoering van de overeenkomst</td>
+                                    <td>Indien u lid wordt gedurende de looptijd van de overeenkomst. Indien u geen lid
+                                        wordt
+                                        worden uw gegevens binnen 4 weken verwijderd.</td>
+                                </tr>
+                                <tr>
+                                    <td>Ledenadministratie</td>
+                                    <td>Voornaam, Achternaam, Geslacht, Adres, Geboortedatum, Geboorteland, Telefoonnummer,
+                                        E-mail adres en Bankgegevens</td>
+                                    <td>Uitvoering van de overeenkomst</td>
+                                    <td>Gedurende de looptijd van de overeenkomst en tot 2 jaar daarna, daarna alleen in de
+                                        financiële administratie voor 7 jaar.</td>
+                                </tr>
+                                <tr>
+                                    <td>Vrijwilligers</td>
+                                    <td>Voornaam, Achternaam, Adres, Telefoonnummer en E-mail adres</td>
+                                    <td>Uitvoering van de overeenkomst</td>
+                                    <td>Gedurende de looptijd van de overeenkomst.</td>
+                                </tr>
+                                <tr>
+                                    <td>Versturen digitale berichten, waaronder nieuwsbrief</td>
+                                    <td>Voornaam, Achternaam en E-mailadres</td>
+                                    <td>Uitvoering van de overeenkomst</td>
+                                    <td>Zolang als men aangemeld is.</td>
+                                </tr>
+                                <tr>
+                                    <td>Versturen berichten van derden, waaronder sponsoren.</td>
+                                    <td>Voornaam, Achternaam en E-mailadres</td>
+                                    <td>Toestemming</td>
+                                    <td>Zolang als men aangemeld is.</td>
+                                </tr>
+                                <tr>
+                                    <td>Om onze digitale dienstverlening te verbeteren</td>
+                                    <td>Cookies (zie voor meer info het sub kopje Cookies)IP-gegevens</td>
+                                    <td>Toestemming</td>
+                                    <td>Iedere keer dat onze website bezocht wordt. Deze gegevens worden zo snel als
+                                        mogelijk
+                                        geanonimiseerd.</td>
+                                </tr>
+                                <tr>
+                                    <td>Ter benadering na beëindiging lidmaatschap. Bijvoorbeeld voor een reünie of
+                                        bijzondere
+                                        gebeurtenis.</td>
+                                    <td>Voornaam, Achternaam, Adres, Telefoonnummer en E-mail adres</td>
+                                    <td>Toestemming</td>
+                                    <td>Zolang als de toestemming niet is ingetrokken.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <p class="text-uppercase fw-bold">Cookies</p>
+                    <p>Een cookie is een klein tekstbestand dat met de pagina's van een website wordt meegestuurd en door de
+                        browser op uw computer, tablet of mobiele telefoon wordt opgeslagen. We gebruiken cookies om u meer
+                        service te bieden bij het bezoeken van onze websites, die daardoor makkelijker worden in gebruik.
+                        Cookies zorgen er bijvoorbeeld voor dat u ingelogd kan blijven op een website of dat uw voorkeuren,
+                        zoals locatie- en taalinstellingen, worden onthouden. We kunnen dankzij cookies zien hoe vaak onze
+                        websites - en welke pagina's - door bezoekers worden bekeken.</p>
+                    <p>Voor het plaatsen van bepaalde cookies moet u eerst toestemming geven, voordat deze cookies mogen
+                        worden gebruikt. Deze toestemming is niet nodig indien en voor zover het technisch noodzakelijke
+                        (functionele) cookies en analytische cookies betreft, in de zin van de toepasselijke wetgeving,
+                        zoals artikel 11.7a lid 3 Telecommunicatiewet en de ePrivacy Verordening.</p>
+                    <p>U kunt uw toestemming voor cookies altijd weer intrekken door uw internetinstellingen te wijzigen.
+                        Meer informatie omtrent het in- en uitschakelen en het verwijderen van cookies kunt u vinden in de
+                        Help-functie van uw browser.</p>
+
+                    <p class="text-uppercase fw-bold">Verstrekking van persoonsgegevens aan derden</p>
+                    <p>Zonder uw toestemming verstrekt de vereniging uw persoonsgegevens alleen aan derden als dit
+                        noodzakelijk is voor de uitvoering van de Overeenkomst die de vereniging met u heeft, tenzij de
+                        vereniging wettelijk verplicht is om uw persoonsgegevens te verstrekken aan deze derden. In de tabel
+                        in het begin van de privacyverklaring treft u een overzicht aan van de situaties waarin
+                        persoonsgegevens aan derden worden verstrekt.</p>
+
+                    <p class="text-uppercase fw-bold">Inzage, correctie en verwijdering van persoonsgegevens</p>
+                    <p>U hebt het recht de vereniging te verzoeken om inzage in uw persoonsgegevens (tenzij de vereniging op
+                        grond van de Algemene Verordening Gegevensbescherming (AVG) niet gehouden is deze inzage te
+                        verschaffen) en om uw gegevens te laten aanvullen, verwijderen of af te schermen. U dient zich bij
+                        een dergelijk verzoek te identificeren. Indien u wenst dat uw gegevens verwijderd worden, zal de
+                        vereniging deze verwijdering doorgeven aan alle andere organisaties die de betreffende gegevens van
+                        de vereniging hebben ontvangen.</p>
+
+                    <p class="text-uppercase fw-bold">Beveiliging persoonsgegevens</p>
+                    <p>De vereniging treft passende maatregelen om uw persoonsgegevens te beveiligen tegen verlies,
+                        ongeautoriseerde toegang, publicatie en onrechtmatige verwerking. Zo zorgen wij dat alleen de
+                        noodzakelijke personen toegang hebben tot uw gegevens, dat de toegang tot de persoonsgegevens
+                        afgeschermd is en dat onze veiligheidsmaatregelen regelmatig gecontroleerd worden.</p>
+
+                    <p class="text-uppercase fw-bold">Minderjarigen</p>
+                    <p>Indien u zich als minderjarige wenst te registreren dient u hiertoe expliciete toestemming van uw
+                        ouder of voogd te overleggen.
+                        Links naar andere websites
+                        De website kan links naar andere websites bevatten. Deze Privacyverklaring is alleen van toepassing
+                        op de websites van de vereniging. Andere websites kunnen hun eigen privacybeleid hanteren. De
+                        vereniging raadt u aan om voor het gebruik van andere websites altijd de betreffende
+                        privacyverklaring van die websites te raadplegen.</p>
+
+                    <p class="text-uppercase fw-bold">Minderjarigen</p>
+                    <p>Indien u zich als minderjarige wenst te registreren dient u hiertoe expliciete toestemming van uw
+                        ouder of voogd te overleggen.
+                        Links naar andere websites
+                        De website kan links naar andere websites bevatten. Deze Privacyverklaring is alleen van toepassing
+                        op de websites van de vereniging. Andere websites kunnen hun eigen privacybeleid hanteren. De
+                        vereniging raadt u aan om voor het gebruik van andere websites altijd de betreffende
+                        privacyverklaring van die websites te raadplegen.</p>
+
+                    <p class="text-uppercase fw-bold">Wijziging van de privacyverklaring</p>
+                    <p>De vereniging past haar privacyverklaring van tijd tot tijd aan om deze up-to-date te houden. Op de
+                        websites zal steeds de meest recente versie van onze Privacyverklaring worden opgenomen. De
+                        vereniging raadt u dan ook aan de Privacyverklaring regelmatig te raadplegen. Bij belangrijke
+                        wijzigingen zal de vereniging er alles aan doen u per e-mail en via de websites te informeren.</p>
+
+                    <p class="text-uppercase fw-bold">Contactgegevens</p>
+                    <p>Indien u vragen hebt over de verwerking van uw persoonsgegevens of de vereniging wilt verzoeken tot
+                        inzage, correctie en/of verwijdering van uw persoonsgegevens, kunt u contact opnemen via
+                        onderstaande contactgegevens:</p>
+                    <p><b>Verantwoordelijke:</b> Secretariaat</p>
+                    <p><b>E-mail:</b> privacy@esbcmenhir.nl</p>
+                    <p><b>Telefoonnummer:</b> 06 - 20377708</p>
+
+                    <p class="text-uppercase fw-bold">Klacht over de verwerking van uw Persoonsgegevens</p>
+                    <p>Natuurlijk helpen wij u ook graag verder als u een klacht heeft over de verwerking van uw
+                        persoonsgegevens. Op grond van de AVG heeft u bovendien het recht om een klacht in te dienen bij de
+                        Autoriteit Persoonsgegevens over onze verwerkingen van uw persoonsgegevens. U kunt hiervoor contact
+                        opnemen met de Autoriteit Persoonsgegevens.</p>
                 </div>
             </div>
         </div>
