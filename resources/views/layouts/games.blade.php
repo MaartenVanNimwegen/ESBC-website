3468810758c3b386d236a24d0a81c8b4e28471e0
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-12-18 16:10:48 +0100

    Styling game blocks

--- resources/views/layouts/games.blade.php
+++ resources/views/layouts/games.blade.php
@@ -1,24 +1,56 @@
-    <div class="col-md-6">
-        <div class="game">
-            <h1 class="text-uppercase fw-bold text-center">Uitslag laatste wedstrijd</h1>
-            <div class="row g-0 game-top-bar">
-                <h1>x12</h1>
+<div class="col-lg-6">
+    <div class="game">
+        <h1 class="text-uppercase fw-bold text-center">Uitslag laatste wedstrijd</h1>
+        <div class="row g-0 game-top-bar">
+            <h1 class="fw-bold">x12</h1>
+        </div>
+        <div class="game-picture-container">
+            <div class="game-picture-bg">
+                <div class="dark-overlay"></div>
+                <img src="{{ asset('storage/images/game-background-1.png') }}">
             </div>
-            <img class="game-picture-bg" src="{{ asset('storage/images/game-background-1.png') }}">
-            <div class="row g-0">
-
+            <div class="game-overlay">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="team">
+                        <h3>ESBC Menhir</h3>
+                        <img src="{{ asset('storage/images/logo.png') }}" alt="ESBC Menhir logo">
+                    </div>
+                    <p class="text-uppercase fw-bold">vs</p>
+                    <div class="team">
+                        <h3>BV Groningen</h3>
+                        <img src="{{ asset('storage/images/logoBVG.png') }}" alt="BVG logo">
+                    </div>
+                </div>
+                <h2 class="text-uppercase fw-bold">18-21</h2>
             </div>
         </div>
     </div>
-    <div class="col-md-6">
-        <div class="game">
-            <h1 class="text-uppercase fw-bold text-center">Uitslag laatste wedstrijd</h1>
-            <div class="row g-0 game-top-bar">
-                <h1>x12</h1>
+</div>
+<div class="col-lg-6">
+    <div class="game">
+        <h1 class="text-uppercase fw-bold text-center">Uitslag laatste wedstrijd</h1>
+        <div class="row g-0 game-top-bar">
+            <h1 class="fw-bold">x12</h1>
+        </div>
+        <div class="game-picture-container">
+            <div class="game-picture-bg">
+                <div class="dark-overlay"></div>
+                <img src="{{ asset('storage/images/game-background-1.png') }}">
             </div>
-            <img class="game-picture-bg" src="{{ asset('storage/images/game-background-1.png') }}">
-            <div class="row g-0">
-
+            <div class="game-overlay">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="team">
+                        <h3>ESBC Menhir</h3>
+                        <img src="{{ asset('storage/images/logo.png') }}" alt="ESBC Menhir logo">
+                    </div>
+                    <p class="text-uppercase fw-bold">vs</p>
+                    <div class="team">
+                        <h3>BV Groningen</h3>
+                        <img src="{{ asset('storage/images/logoBVG.png') }}" alt="BVG logo">
+                    </div>
+                </div>
+                <h2 class="text-uppercase fw-bold">18-21</h2>
             </div>
         </div>
     </div>
+</div>
