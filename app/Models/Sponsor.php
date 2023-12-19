c838728f1aad4daddbc11dbc249cf41ff4c3eaee
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2023-12-19 18:14:05 +0100

    add link to sponsor

--- app/Models/Sponsor.php
+++ app/Models/Sponsor.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Sponsor extends Model {
+class Sponsor extends Model
+{
     protected $table = 'sponsor';
 
     protected $fillable = [
         'title',
         'picture_location',
+        'link'
     ];
 }
