6a6453fd9d650ce96800a9a50eb1f588bfe55bdc
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2024-01-10 12:14:01 +0100

    WIP games page

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Game;
+use DateTime;
+
+class GameController extends Controller
+{
+    public function Index()
+    {
+        $response = Http::get('http://db.basketball.nl/db/json/wedstrijd.pl?clb_ID=341');
+        if ($response->successful()) {
+            // Process the data as needed
+            $data = json_decode($response->body(), true);
+            $games = [];
+            $currentDate = new DateTime();
+            foreach ($data['wedstrijden'] as $game) {
+                $playDate = new DateTime($game['datum']);
+                if ($playDate >= $currentDate) {
+                    $gameModel = new Game();
+                    $gameModel->datum = date_format($playDate, 'd-m-Y');
+                    $gameModel->tijd = date_format($playDate, 'H:i');
+                    $gameModel->thuisPloeg = $game['thuis_ploeg'];
+                    $gameModel->uitPloeg = $game['uit_ploeg'];
+                    $games[] = $gameModel;
+                }
+            }
+            return view('wedstrijden', ['upcommingGames' => $games]);
+        } else {
+            return view('wedstrijden', ['upcommingGames' => 'error']);
+        }
+    }
+}
--- resources/views/wedstrijden.blade.php
+++ resources/views/wedstrijden.blade.php
@@ -4,7 +4,35 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>test wedstrijden</h1>
+                @if ($upcommingGames !== 'error')
+                    <h1>Wedstrijden</h1>
+                    <div class="table-responsive">
+
+                        <table class="table table-striped">
+                            <thead class="bg-head">
+                                <tr>
+                                    <th>Datum</th>
+                                    <th>Tijd</th>
+                                    <th>Thuisteam</th>
+                                    <th>Uitteam</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-text">
+                                @foreach ($upcommingGames as $upcommingGame)
+                                    <tr>
+                                        <td>{{ $upcommingGame->datum }}</td>
+                                        <td>{{ $upcommingGame->tijd }}</td>
+                                        <td>{{ $upcommingGame->thuisPloeg }}</td>
+                                        <td>{{ $upcommingGame->uitPloeg }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                @else
+                    <h1>Er is iets fout gegaan bij het ophalen van de wedstrijden!</h1>
+                    <h1>Probeer het later nog eens.</h1>
+                @endif
             </div>
         </div>
     </div>
