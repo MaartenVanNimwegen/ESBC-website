ad9109ccf517a204b34f7db7de63068dd571d5d6
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2024-01-22 14:51:09 +0100

    Case of images

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -74,11 +74,99 @@ class HomeController extends Controller
     {
         switch ($ploegName) {
             case (stripos($ploegName, 'esbc menhir') !== false):
-                return "storage/images/logos/logoMenhir_Web.svg";
+                return "storage/images/logos/menhir_logo.svg";
                 break;
 
             case (stripos($ploegName, 'groningen') !== false):
-                return "storage/images/logos/bvgGroningen.png";
+                return "storage/images/logos/groningen_logo.png";
+                break;
+
+            case (stripos($ploegName, 'hornets') !== false):
+                return "storage/images/logos/hornets_logo.png";
+                break;
+
+            case (stripos($ploegName, 'arrows') !== false):
+                return "storage/images/logos/arrows_logo.png";
+                break;
+
+            case (stripos($ploegName, 'ceres') !== false):
+                return "storage/images/logos/ceres_logo.png";
+                break;
+
+            case (stripos($ploegName, 'assist') !== false):
+                return "storage/images/logos/assist_logo.png";
+                break;
+
+            case (stripos($ploegName, 'leeuwarden') !== false):
+                return "storage/images/logos/leeuwarden_logo.png";
+                break;
+
+            case (stripos($ploegName, 'H.S.V.') !== false):
+                return "storage/images/logos/hsv_logo.png";
+                break;
+
+            case (stripos($ploegName, 'moestasj') !== false):
+                return "storage/images/logos/moestasj_logo.png";
+                break;
+
+            case (stripos($ploegName, 'celeritas') !== false):
+                return "storage/images/logos/celeritas_logo.png";
+                break;
+
+            case (stripos($ploegName, 'rhino') !== false):
+                return "storage/images/logos/rhino_logo.png";
+                break;
+
+            case (stripos($ploegName, 'falcons') !== false):
+                return "storage/images/logos/falcons_logo.png";
+                break;
+
+            case (stripos($ploegName, 'penta') !== false):
+                return "storage/images/logos/penta_logo.png";
+                break;
+
+            case (stripos($ploegName, 'oaters') !== false):
+                return "storage/images/logos/oaters_logo.png";
+                break;
+
+            case (stripos($ploegName, 'mustangs') !== false):
+                return "storage/images/logos/mustangs_logo.png";
+                break;
+
+            case (stripos($ploegName, 'trojans') !== false):
+                return "storage/images/logos/trojans_logo.png";
+                break;
+
+            case (stripos($ploegName, 'lions') !== false):
+                return "storage/images/logos/lions_logo.png";
+                break;
+
+            case (stripos($ploegName, 'bedum') !== false):
+                return "storage/images/logos/bedum_logo.png";
+                break;
+
+            case (stripos($ploegName, 'dyna') !== false):
+                return "storage/images/logos/dyna_logo.png";
+                break;
+
+            case (stripos($ploegName, 'scylla') !== false):
+                return "storage/images/logos/scylla_logo.png";
+                break;
+
+            case (stripos($ploegName, 'uilen') !== false):
+                return "storage/images/logos/uilen_logo.png";
+                break;
+
+            case (stripos($ploegName, 'M.A.C.') !== false):
+                return "storage/images/logos/mac_logo.png";
+                break;
+
+            case (stripos($ploegName, 'quintas') !== false):
+                return "storage/images/logos/quintas.png";
+                break;
+
+            case (stripos($ploegName, 'five') !== false):
+                return "storage/images/logos/five_logo.png";
                 break;
 
             default:
