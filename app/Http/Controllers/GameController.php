6bb33ffa7c614e5c1299cc1b697077bb12369ffc
MaartenVanNimwgen <maartenvannimwegen@hotmail.com>
2024-03-12 19:57:56 +0100

    Optimalisatie

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -11,22 +11,18 @@ class GameController extends Controller
 {
     public function ViewGame()
     {
-        $response = Http::get('http://db.basketball.nl/db/json/wedstrijd.pl?clb_ID=341');
+        $response = Http::get('http://db.basketball.nl/db/json/wedstrijd.pl?clb_ID=341&dagen_terug=1');
         if ($response->successful()) {
-            // Process the data as needed
             $data = json_decode($response->body(), true);
             $games = [];
-            $currentDate = new DateTime();
             foreach ($data['wedstrijden'] as $game) {
                 $playDate = new DateTime($game['datum']);
-                if ($playDate >= $currentDate) {
-                    $gameModel = new Game();
-                    $gameModel->datum = date_format($playDate, 'd-m-Y');
-                    $gameModel->tijd = date_format($playDate, 'H:i');
-                    $gameModel->thuisPloeg = $game['thuis_ploeg'];
-                    $gameModel->uitPloeg = $game['uit_ploeg'];
-                    $games[] = $gameModel;
-                }
+                $gameModel = new Game();
+                $gameModel->datum = date_format($playDate, 'd-m-Y');
+                $gameModel->tijd = date_format($playDate, 'H:i');
+                $gameModel->thuisPloeg = $game['thuis_ploeg'];
+                $gameModel->uitPloeg = $game['uit_ploeg'];
+                $games[] = $gameModel;
             }
             return view('wedstrijden', ['upcommingGames' => $games]);
         } else {
